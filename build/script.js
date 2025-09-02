ad6520dadc1ff76dbd0aed57381e2ab93e4a4beb
TafitasoaThierry <nattafitasoa18@gmail.com>
2025-09-02 22:47:20 +0300

    Initial commit

---
+++ build/script.js
@@ -0,0 +1,5 @@
+// let profileTitle = document.getElementsByClassName("profile-title")[0] as HTMLElement;
+// profileTitle.style.display = "none";
+// setTimeout(() => {
+//     profileTitle.style.display = "inherit";
+// }, 2000)
---
+++ ts/script.ts
@@ -0,0 +1,7 @@
+// let profileTitle = document.getElementsByClassName("profile-title")[0] as HTMLElement;
+// profileTitle.style.display = "none";
+
+// setTimeout(() => {
+//     profileTitle.style.display = "inherit";
+// }, 2000)
+
