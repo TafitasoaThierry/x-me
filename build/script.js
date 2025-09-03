425528a91224f6c67a9cd04ede4e847821ad2959
TafitasoaThierry <nattafitasoa18@gmail.com>
2025-09-04 01:18:08 +0300

    Loading and welcome modal

--- build/script.js
+++ build/script.js
@@ -1,28 +1,45 @@
+var container = document.getElementsByClassName("container")[0];
+var welcome = document.getElementsByClassName("welcome")[0];
+var presentation = document.getElementsByClassName("presentation")[0];
 document.getElementById("year").innerHTML = (new Date().getFullYear() - 2023).toString();
-/*
-let kisaka:any = document.getElementsByClassName("kisaka")[0] as HTMLElement;
-let mandeha:boolean = true;
-let miverina:boolean = false;
-let fetra:number = 300;
-let fiatombohany:number = 0;
-let x:number = fiatombohany;
-
-setInterval(() => {
-    console.log(x);
-    if((mandeha) && (x != fetra)) {
-        kisaka.scrollLeft = x;
-        x = x + 1;
-    }else if((mandeha) && (x == fetra)) {
-        mandeha = false;
-        miverina = true;
-        kisaka.scrollLeft = x;
-    }else if((miverina) && (x != fiatombohany)) {
-        kisaka.scrollLeft = x;
-        x = x - 1;
-    }else {
-        mandeha = true;
-        miverina = false;
-        kisaka.scrollLeft = x;
-    }
-}, 50)
-*/
+document.body.style.height = "100vh";
+container.style.display = "none";
+presentation.style.display = "none";
+setTimeout(function () {
+    presentation.style.display = "inherit";
+    setTimeout(function () {
+        welcome.style.display = "none";
+        container.style.display = "inherit";
+        document.body.style.height = "auto";
+    }, 4000);
+}, 4000);
+startSlideShow();
+function startSlideShow() {
+    var kisaka = document.getElementsByClassName("image-presentation")[1];
+    var mandeha = true;
+    var miverina = false;
+    var fetra = 505;
+    var fiatombohany = 0;
+    var x = fiatombohany;
+    setInterval(function () {
+        fetra = window.innerWidth <= 340 ? 305 : 505;
+        if ((mandeha) && (x != fetra)) {
+            kisaka.scrollLeft = x;
+            x++;
+        }
+        else if ((mandeha) && (x == fetra)) {
+            mandeha = false;
+            miverina = true;
+            kisaka.scrollLeft = x;
+        }
+        else if ((miverina) && (x != fiatombohany)) {
+            kisaka.scrollLeft = x;
+            x--;
+        }
+        else {
+            mandeha = true;
+            miverina = false;
+            kisaka.scrollLeft = x;
+        }
+    }, 30);
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,29 +1,46 @@
+let container = document.getElementsByClassName("container")[0] as HTMLElement;
+let welcome = document.getElementsByClassName("welcome")[0] as HTMLElement;
+let presentation = document.getElementsByClassName("presentation")[0] as HTMLElement;
+
 document.getElementById("year").innerHTML = (new Date().getFullYear() - 2023).toString();
+document.body.style.height = "100vh";
+container.style.display = "none";
+presentation.style.display = "none";
 
-/*
-let kisaka:any = document.getElementsByClassName("kisaka")[0] as HTMLElement;
-let mandeha:boolean = true;
-let miverina:boolean = false;
-let fetra:number = 300;
-let fiatombohany:number = 0;
-let x:number = fiatombohany;
+setTimeout(() => {
+    presentation.style.display = "inherit";
+    setTimeout(() => {
+        welcome.style.display = "none";
+        container.style.display = "inherit";
+        document.body.style.height = "auto";
+    }, 4000)
+}, 4000)
+startSlideShow();
 
-setInterval(() => {
-    console.log(x);
-    if((mandeha) && (x != fetra)) {
-        kisaka.scrollLeft = x;
-        x = x + 1;
-    }else if((mandeha) && (x == fetra)) {
-        mandeha = false;
-        miverina = true;
-        kisaka.scrollLeft = x;
-    }else if((miverina) && (x != fiatombohany)) {
-        kisaka.scrollLeft = x;
-        x = x - 1;
-    }else {
-        mandeha = true;
-        miverina = false;
-        kisaka.scrollLeft = x;
-    }
-}, 50)
-*/
+function startSlideShow() {
+    let kisaka:any = document.getElementsByClassName("image-presentation")[1] as HTMLElement;
+    let mandeha:boolean = true;
+    let miverina:boolean = false;
+    let fetra:number = 505;
+    let fiatombohany:number = 0;
+    let x:number = fiatombohany;
+
+    setInterval(() => {
+        fetra = window.innerWidth <= 340 ? 305 : 505;
+        if((mandeha) && (x != fetra)) {
+            kisaka.scrollLeft = x;
+            x++;
+        }else if((mandeha) && (x == fetra)) {
+            mandeha = false;
+            miverina = true;
+            kisaka.scrollLeft = x;
+        }else if((miverina) && (x != fiatombohany)) {
+            kisaka.scrollLeft = x;
+            x--;
+        }else {
+            mandeha = true;
+            miverina = false;
+            kisaka.scrollLeft = x;
+        }
+    }, 30)
+}
