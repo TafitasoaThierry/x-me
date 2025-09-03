7eef7a2471170552a66d7000aad3b1cab99f9ca9
TafitasoaThierry <nattafitasoa18@gmail.com>
2025-09-03 17:43:39 +0300

    Kely sisa

--- build/script.js
+++ build/script.js
@@ -1,5 +1,28 @@
-// let profileTitle = document.getElementsByClassName("profile-title")[0] as HTMLElement;
-// profileTitle.style.display = "none";
-// setTimeout(() => {
-//     profileTitle.style.display = "inherit";
-// }, 2000)
+document.getElementById("year").innerHTML = (new Date().getFullYear() - 2023).toString();
+/*
+let kisaka:any = document.getElementsByClassName("kisaka")[0] as HTMLElement;
+let mandeha:boolean = true;
+let miverina:boolean = false;
+let fetra:number = 300;
+let fiatombohany:number = 0;
+let x:number = fiatombohany;
+
+setInterval(() => {
+    console.log(x);
+    if((mandeha) && (x != fetra)) {
+        kisaka.scrollLeft = x;
+        x = x + 1;
+    }else if((mandeha) && (x == fetra)) {
+        mandeha = false;
+        miverina = true;
+        kisaka.scrollLeft = x;
+    }else if((miverina) && (x != fiatombohany)) {
+        kisaka.scrollLeft = x;
+        x = x - 1;
+    }else {
+        mandeha = true;
+        miverina = false;
+        kisaka.scrollLeft = x;
+    }
+}, 50)
+*/
--- ts/script.ts
+++ ts/script.ts
@@ -1,7 +1,29 @@
-// let profileTitle = document.getElementsByClassName("profile-title")[0] as HTMLElement;
-// profileTitle.style.display = "none";
+document.getElementById("year").innerHTML = (new Date().getFullYear() - 2023).toString();
 
-// setTimeout(() => {
-//     profileTitle.style.display = "inherit";
-// }, 2000)
+/*
+let kisaka:any = document.getElementsByClassName("kisaka")[0] as HTMLElement;
+let mandeha:boolean = true;
+let miverina:boolean = false;
+let fetra:number = 300;
+let fiatombohany:number = 0;
+let x:number = fiatombohany;
 
+setInterval(() => {
+    console.log(x);
+    if((mandeha) && (x != fetra)) {
+        kisaka.scrollLeft = x;
+        x = x + 1;
+    }else if((mandeha) && (x == fetra)) {
+        mandeha = false;
+        miverina = true;
+        kisaka.scrollLeft = x;
+    }else if((miverina) && (x != fiatombohany)) {
+        kisaka.scrollLeft = x;
+        x = x - 1;
+    }else {
+        mandeha = true;
+        miverina = false;
+        kisaka.scrollLeft = x;
+    }
+}, 50)
+*/
